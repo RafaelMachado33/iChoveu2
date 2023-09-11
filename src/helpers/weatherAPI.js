41eb6682a561bbe01aaeef8e26e585e0cdc9cfb0
Rafael Machado <machadorafael27@gmail.com>
2023-09-11 19:54:03 -0300

    exercise 1 check

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,13 +1,15 @@
-const meuToken = import.meta.env.VITE_TOKEN
-
-//console.log(dados.json())  pq não finciona o consoe.log()?
+const meuToken = import.meta.env.VITE_TOKEN;
 
 export const searchCities = (term) => {
-    fetch(`api.weatherapi.com/v1/current.json?key=${meuToken}&q=${term}&aqi=no`)
-        .then((response) => response.json())
-        .then((data) => console.log(data));
+  fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${meuToken}&q=${term}`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.length === 0) {
+        window.alert('Nenhuma cidade encontrada');
+      }
+    });
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
-    //   seu código aqui
+  //   seu código aqui
 };
